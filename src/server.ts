e54eceb95f20e5eb4626b0717e5840d2f8ef8a12
Evandro Passaia <evandrobaraka@gmail.com>
2026-03-23 00:37:18 -0300

    configurações iniciais do express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-const name = 'teste';
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/movies', (req, res) => {
+    res.send('Listagem de filmes');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
