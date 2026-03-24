4ad37c75cd778cf5fc5835d12be1fa3e6ec296d9
Evandro Passaia <evandrobaraka@gmail.com>
2026-03-24 19:07:02 -0300

    feat: adicionadas rotas de listagem de filmes e de seleção de filme por id

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,34 @@
-const express = require('express');
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
 
-app.get('/movies', (req, res) => {
-    res.send('Listagem de filmes');
+app.get('/movies', async (req, res) => {
+    try {
+        const movies = await prisma.movie.findMany();
+        res.json(movies);
+    } catch (error) {
+        console.error('Erro detalhado:', error);
+        res.status(500).json({ error: 'Erro ao buscar filmes' });
+    }
+});
+
+app.get('/movies/:id', async (req, res) => {
+    const movieId = parseInt(req.params.id)
+
+    try {
+        const movie = await prisma.movie.findMany({
+            where: {
+                id: movieId
+            }
+        });
+        res.json(movie);
+    } catch (error) {
+        console.error('Erro detalhado:', error);
+        res.status(500).json({ error: 'Erro ao buscar filmes' });
+    }
 });
 
 app.listen(port, () => {
